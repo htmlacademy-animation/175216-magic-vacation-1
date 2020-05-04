3e33541f9ca0fb0ad5ac175a8516ea07311f6c44
rawle51 <costa08@mail.ru>
2020-05-04 13:55:57 +0300

    edit script for animated rules button

--- source/js/modules/rules.js
+++ source/js/modules/rules.js
@@ -1,8 +1,30 @@
+const RULES_HASH = `#rules`;
+
 export default () => {
   const button = document.querySelector(`.rules__link`);
-  const lastRule = document.querySelector(`.rules__list li:last-child`);
+  const lastRule = document.querySelector(`.rules__list li:last-child p`);
 
-  lastRule.addEventListener(`animationend`, function () {
+  function addBtnAnimateClass() {
     button.classList.add(`rules__link--show`);
+  }
+
+  function removeBtnAnimateClass() {
+    button.classList.remove(`rules__link--show`);
+  }
+
+  function controlBtnAnimateClass() {
+    if (location.hash === RULES_HASH) {
+      return lastRule.addEventListener(`animationend`, addBtnAnimateClass);
+    }
+
+    return removeBtnAnimateClass();
+  }
+
+  // init if the first visit page is #rules
+  controlBtnAnimateClass();
+
+  // listener for page changes
+  window.addEventListener(`popstate`, function () {
+    controlBtnAnimateClass();
   });
 };
