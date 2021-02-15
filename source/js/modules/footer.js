10c69712ba59e53a16b8a2fcb0ae5e3498988c35
Konstantin Rawle <costa08@mail.ru>
2021-02-16 00:23:05 +0300

    add mobile footer animation

--- source/js/modules/footer.js
+++ source/js/modules/footer.js
@@ -7,7 +7,9 @@ export default () => {
         let footer = footerTogglers[i].parentNode;
         if (footer.classList.contains(`screen__footer--full`)) {
           footer.classList.remove(`screen__footer--full`);
+          footer.classList.add(`screen__footer--small`);
         } else {
+          footer.classList.remove(`screen__footer--small`);
           footer.classList.add(`screen__footer--full`);
         }
       });
